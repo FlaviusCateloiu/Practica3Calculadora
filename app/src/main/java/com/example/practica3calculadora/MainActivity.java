34c433159c86bdf48daa29dc7138b0adf59076c3
flaars <fcateloiu08@gmail.com>
2022-10-11 16:45:05 +0200

    Terminado aun que me falta poner el menos.

--- app/src/main/java/com/example/practica3calculadora/MainActivity.java
+++ app/src/main/java/com/example/practica3calculadora/MainActivity.java
@@ -3,12 +3,118 @@ package com.example.practica3calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean sintaxError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickResult(View view) {
+        String contenido;
+        int positionPlus, num1, num2;
+        TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+
+        contenido = mostrar.getText().toString();
+        if (contenido.contains("+")) {
+            positionPlus = contenido.indexOf("+");
+            try {
+                num1 = Integer.parseInt(contenido.substring(0, positionPlus));
+                num2 = Integer.parseInt(contenido.substring(positionPlus, contenido.length()));
+                mostrar.setText("" + (num1 + num2));
+            } catch (Exception e) {
+                mostrar.setText("Sintax Error");
+                this.sintaxError = true;
+            }
+        }
+    }
+
+    public void clickNumber1(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "1");
+        }
+    }
+
+    public void clickNumber2(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "2");
+        }
+    }
+
+    public void clickNumber3(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "3");
+        }
+    }
+
+    public void clickNumber4(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "4");
+        }
+    }
+
+    public void clickNumber5(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "5");
+        }
+    }
+
+    public void clickNumber6(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "6");
+        }
+    }
+
+    public void clickNumber7(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "7");
+        }
+    }
+
+    public void clickNumber8(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "8");
+        }
+    }
+
+    public void clickNumber9(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "9");
+        }
+    }
+
+    public void clickNumber0(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "0");
+        }
+    }
+
+    public void clickPlus(View view) {
+        if (!sintaxError) {
+            TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+            mostrar.setText(mostrar.getText() + "+");
+        }
+    }
+
+    public void clickCBorrar(View view) {
+        TextView mostrar = (TextView) findViewById(R.id.tFResultado);
+        mostrar.setText("");
+        this.sintaxError = false;
     }
 }
