5b7f0cbe2656a2f043f4a10dc3e92a65925a3fe8
flaars <fcateloiu08@gmail.com>
2022-10-19 20:02:48 +0200

    Terminado con exito.

--- app/src/main/java/com/example/practica3calculadora/Calculadora.java
+++ app/src/main/java/com/example/practica3calculadora/Calculadora.java
@@ -10,20 +10,24 @@ public class Calculadora {
                 num1 = cadena.substring(0, posMas);
                 num2 = cadena.substring(posMas + 1);
                 return String.valueOf(Integer.parseInt(calculate(num1)) + Integer.parseInt(calculate(num2)));
-            } else if (cadena.contains("-")) {
-                posMas = cadena.lastIndexOf("-");
-                num1 = cadena.substring(0, posMas);
-                num2 = cadena.substring(posMas + 1);
-                if (Integer.parseInt(calculate(num1)) < Integer.parseInt(calculate(num2))) {
-                    return "-" + String.valueOf(Integer.parseInt(calculate(num2)) - Integer.parseInt(calculate(num1)));
-                } else {
-                    return String.valueOf(Integer.parseInt(calculate(num1)) - Integer.parseInt(calculate(num2)));
-                }
             } else if (cadena.contains("*")) {
                 posMas = cadena.indexOf("*");
                 num1 = cadena.substring(0, posMas);
                 num2 = cadena.substring(posMas + 1);
                 return String.valueOf(Integer.parseInt(calculate(num1)) * Integer.parseInt(calculate(num2)));
+            } else if (cadena.contains("-")) {
+                posMas = cadena.lastIndexOf("-");
+                num1 = cadena.substring(0, posMas);
+                num2 = cadena.substring(posMas + 1);
+                if (num1.length() > 2 && num1.charAt(2) == '-') {
+                    return String.valueOf(Integer.parseInt(calculate(num1)) + Integer.parseInt(calculate(num2)));
+                } else {
+                    if (Integer.parseInt(calculate(num1)) < Integer.parseInt(calculate(num2))) {
+                        return "-" + String.valueOf(Integer.parseInt(calculate(num2)) - Integer.parseInt(calculate(num1)));
+                    } else {
+                        return String.valueOf(Integer.parseInt(calculate(num1)) - Integer.parseInt(calculate(num2)));
+                    }
+                }
             } else {
                 if (cadena.isEmpty()) {
                     return "0";
